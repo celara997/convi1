aed74388889a8400b3cd5abbb5670b2ad5786de0
Cesar Lara <celara@imed.cl>
2023-12-22 16:39:29 -0300

    test

--- pages/ui/Casos.vue
+++ pages/ui/Casos.vue
@@ -6,8 +6,7 @@ import "bootstrap/dist/css/bootstrap.css";
   <div class="jumbotron">
     <h1 class="display-4">Mis Casos</h1>
     <p class="lead">
-      This is a simple hero unit, a simple jumbotron-style component for calling
-      extra attention to featured content or information.
+          
     </p>
     <hr class="my-4" />
     <v-col cols="12" md="12">
--- server/api/denuncia/create.post.ts
+++ server/api/denuncia/create.post.ts
@@ -1,7 +1,9 @@
 import { denuncia } from "../../models/denuncias.model"
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
+    console.log(body)
     const newelement = new denuncia({...body});
     newelement.save();
+
     return `Denuncia creada`
 })
