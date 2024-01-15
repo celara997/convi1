bf2c805aa570e4a353939d8938d56148c0eefd9d
Cesar Lara <celara@imed.cl>
2024-01-15 16:11:58 -0300

    fix find denuncia

--- server/api/denuncia/index.get.ts
+++ server/api/denuncia/index.get.ts
@@ -1,7 +1,9 @@
 import { denuncia } from "../../models/denuncias.model";
+import { encargado } from "../../models/encargado.model";
 
 
 export default defineEventHandler(async (event) => {
+    await encargado.findOne();
     const denuncias = await denuncia.find().populate("encargado");
     if (!denuncia || denuncia.length === 0) throw createError({
         statusCode: 500,
